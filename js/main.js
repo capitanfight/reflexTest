cb6beb776d85fa12a0083a92cbe1265508fda7ec
capitanfight <cinielia0719@gmail.com>
2024-11-24 11:04:32 +0100

    si

--- js/main.js
+++ js/main.js
@@ -1,16 +1,28 @@
-import { Game } from "./game.js"
+const point_tag = document.getElementById("pt");
+const start_btn = document.getElementById("start-btn");
 
-const point_tag = document.getElementById("point-tag");
+const game_duration = 10000;
+const target_spec = {
+    innerHeight: 50,
+    innerWidth: 50,
+}
+
+let intervalID;
+
+let targets = [];
 
 const spawn_target = () => {
     let t = document.createElement("div");
 
+    
     t.classList.add("target");
-    t.onclick = remove_target;
-    t.style.top = (Math.random() * window.innerHeight) + "px"; 
-    t.style.left = (Math.random() * window.innerWidth) + "px";
+    t.addEventListener("mousedown", remove_target);
+    t.removeEventListener("click", remove_target);
+    t.style.top = Math.max(Math.random() * window.innerHeight - target_spec.innerHeight, 0) + "px"; 
+    t.style.left = Math.max(Math.random() * window.innerWidth - target_spec.innerWidth, 0) + "px";
 
     document.body.appendChild(t);
+    targets.push(t)
 }
 
 const remove_target = (e) => {
@@ -19,7 +31,24 @@ const remove_target = (e) => {
 }
 
 function start() {
-    setInterval(spawn_target, 300)
+    start_btn.style.display = "none";
+
+    intervalID = setInterval(spawn_target, 300);
+    setTimeout(end, game_duration);
 }
 
-start()
+function end() {
+    start_btn.style.display = "block";
+
+    targets.forEach(t => {
+        try {
+            document.body.removeChild(t);
+        } catch (e) {
+            console.error(e)
+        }
+    });
+
+    clearInterval(intervalID);
+}
+
+start_btn.addEventListener("click", start);
